03a820272130e503500a4dc544f602e805c9c431
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2022-05-02 23:46:18 +0900

    add galGal6

--- juicer-1.6/restriction_sites/script.sh
+++ juicer-1.6/restriction_sites/script.sh
@@ -1,7 +1,12 @@
-for build in mm39 #galGal5 #ce10 #hg19 hg38 mm10
+for build in #mm39 #galGal5 #ce10 #hg19 hg38 mm10
 do
     genome=/work/Database/UCSC/$build/genome.fa
     for enzyme in HindIII DpnII MboI; do
 	python ../misc/generate_site_positions.py $enzyme $build $genome &
     done
+done
+
+genome=/work/Database/Database_fromDocker/Ensembl-GRCg6a/genome.fa
+for enzyme in HindIII DpnII MboI; do
+    python ../misc/generate_site_positions.py $enzyme galGal6 $genome &
 done
